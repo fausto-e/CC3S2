7c26176fe8cd8c0772eb7b29f328b3f62e2484b0
fausto-e <fausto4412711@gmail.com>
2025-09-07 19:34:12 -0500

    Add Makefile

--- Proyectos/Prueba_entrada_CC3S2/seccion1_cli_automatizacion/scripts/syscheck.sh
+++ Proyectos/Prueba_entrada_CC3S2/seccion1_cli_automatizacion/scripts/syscheck.sh
@@ -7,7 +7,6 @@ set -euo pipefail
 
 trap 'echo "Error en la línea $LINENO; $BASH_COMMAND"' ERR
 
-trap 'echo "Finalizando ejecución"' EXIT
 
 # Crear reporte para http.txt
 if [ ! -d "$REPORT_DIR" ]; then
@@ -15,6 +14,10 @@ if [ ! -d "$REPORT_DIR" ]; then
 fi
 
 curl -Is https://example.com > "$REPORT_DIR/http.txt"
+
+echo -e "\nComentarios:\nLa respuesta usa HTTP/2 (200 OK), entrega HTML y controla caché con ETag, Last-Modified y Cache-Control (~24h). \
+El header Date marca cuándo respondió el servidor y Alt-Svc indica soporte de HTTP/3." >> "$REPORT_DIR/http.txt"
+
 
 # Crear reporte para dns.txt
 echo "Registro A para example.com:" > "$REPORT_DIR/dns.txt"
@@ -26,9 +29,14 @@ dig AAAA example.org +noall +answer >> "$REPORT_DIR/dns.txt"
 echo -e "\nRegistro MX para example.com:" >> "$REPORT_DIR/dns.txt"
 dig MX example.com +noall +answer >> "$REPORT_DIR/dns.txt"
 
+echo "En DNS, el TTL define cuánto tiempo las respuestas (A, AAAA, MX) pueden guardarse en caché antes de requerir nueva consulta."  >> dns.txt
+
 echo "Versión TLS observada:" > "$REPORT_DIR/tls.txt"
 curl -Iv https://example.com 2>&1 | grep "TLS" >> "$REPORT_DIR/tls.txt"
 
 ss -tuln > "$REPORT_DIR/sockets.txt"
 echo -e "\nRiesgos: puertos abiertos sin firewall o servicios innecesarios exponen la máquina a ataques." >> "$REPORT_DIR/sockets.txt"
 
+echo "======================================================"
+echo -e "\tsyscheck.sh finalizado correctamente"
+echo "======================================================"
